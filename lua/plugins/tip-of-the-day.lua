2543549d3473721cba1a7554edaa2387be8077b0
Mico Rigunay <micorigunay@Micos-M3-MacBook-Air.local>
2024-09-24 06:55:08 +0800

    Added tip of the day and dashboard

---
+++ lua/plugins/tip-of-the-day.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		dependencies = { "rcarriga/nvim-notify" },
+		config = function(_, opts)
+			local job = require("plenary.job")
+			job:new({
+				command = "curl",
+				args = { "https://vtip.43z.one" },
+				on_exit = function(j, exit_code)
+					local res = table.concat(j:result())
+					if exit_code ~= 0 then
+						res = "Error fetching tip: " .. res
+					end
+					-- Use vim.schedule to ensure this runs in the main thread
+					vim.schedule(function()
+						require("notify")(res)
+					end)
+				end,
+			}):start()
+		end,
+	},
+}
