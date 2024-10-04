3826de2d2ea798c06ae20fc282d1605144ee1511
Mico Rigunay <micorigunay@Micos-M3-MacBook-Air.local>
2024-10-04 11:10:46 +0800

    catppuccin generalization

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,27 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    integrations = {
+        neotree = true,
+        lualine = true,
+    },
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+            default_integrations = true,
+            integrations = {
+                neotree = true,
+                treesitter = true,
+            },
+            -- custom_highlights = function(colors)
+            --     return {
+            --         NeoTreeNormal = { bg = "#191926" },
+            --         NeoTreeNormalNC = { bg = "#191926" },
+            --     }
+            -- end,
+        })
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
